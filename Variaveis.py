3c904a97b5eb321ebbd4f2050b7d82f2335a1683
EddieNine <believefixed@hotmail.com>
2025-05-07 16:37:24 -0300

    variaveis

---
+++ Variaveis.py
@@ -0,0 +1,42 @@
+
+# Strings
+primeiro_nome = "Edcarlos"
+print(f"Olá, {primeiro_nome}")
+
+comida = "Pizza"
+print(f"Você gosta de {comida}")
+
+# Integers
+idade = 26
+print(f"Você tem {idade} anos")
+
+quantidade = 3
+print(f"Você está comprando {quantidade} itens")
+
+numero_de_estudantes = 30
+print(f"Sua classe tem {numero_de_estudantes} estudantes")
+
+# Floats
+preço = 10.99
+print(f"O preço é R${preço}")
+
+media = 6.9
+print(f"Sua media é {media}")
+
+distancia = 5.5
+print(f"você correu {distancia}km")
+
+# Booleans
+es_estudante = False
+print(f"Você é um estudante? {es_estudante}")
+
+if es_estudante:
+    print("você é um estudante")
+else:
+    print("Você não é um estudante")
+
+esta_online = False
+if esta_online:
+    print("Você está online")
+else:
+    print("Você não está online")
